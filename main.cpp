e3b56f52c0cf85b88b0ea83030d146be1577515f
Brito <jmbmaluno@gmail.com>
2025-11-06 15:44:29 -0300

    comecei

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <new>
+#include <sstream>
+
+int nim(n){
+    int* v;
+
+    v = new int[n];
+
+    v[0] = 0;
+    v[1] = 2;
+    v[2] = 3;
+    v[3] = 1;
+    
+}
+
+int main(){
+
+}
